f6da7c39b1fd2351a138ca8a06f064a7ae6627a3
Aaron Whang and Jessica Munoz <aaron@>
2019-06-04 14:45:38 -0700

    Added individual toggling flash cards that may toggle between name and answer.

---
+++ js/scripts.js
@@ -0,0 +1,104 @@
+$(document).ready(function(){
+
+  $("p.Javascript").click(function(){
+    $(".Javascript-definition").toggle();
+    $(".Javascript-name").toggle();
+  });
+
+  $("p.Operators").click(function(){
+    $(".Operators-definition").toggle();
+    $(".Operators-name").toggle();
+  });
+
+  $("p.Variables").click(function(){
+    $(".Variables-definition").toggle();
+    $(".Variables-name").toggle();
+  });
+
+  $("p.Variable-nam-con").click(function(){
+    $(".Variable-nam-con-definition").toggle();
+    $(".Variable-nam-con-name").toggle();
+  });
+
+  $("p.Functions").click(function(){
+    $(".Functions-definition").toggle();
+    $(".Functions-name").toggle();
+  });
+
+  $("p.Methods").click(function(){
+    $(".Methods-definition").toggle();
+    $(".Methods-name").toggle();
+  });
+
+  $("p.Arguments").click(function(){
+    $(".Arguments-definition").toggle();
+    $(".Arguments-name").toggle();
+  });
+
+  $("p.Parameters").click(function(){
+    $(".Parameters-definition").toggle();
+    $(".Parameters-name").toggle();
+  });
+
+  $("p.Return").click(function(){
+    $(".Return-definition").toggle();
+    $(".Return-name").toggle();
+  });
+
+  $("p.Chaining").click(function(){
+    $(".Chaining-definition").toggle();
+    $(".Chaining-name").toggle();
+  });
+
+  $("p.Strings").click(function(){
+    $(".Strings-definition").toggle();
+    $(".Strings-name").toggle();
+  });
+
+  $("p.Booleans").click(function(){
+    $(".Booleans-definition").toggle();
+    $(".Booleans-name").toggle();
+  });
+
+  $("p.Undefined").click(function(){
+    $(".Undefined-definition").toggle();
+    $(".Undefined-name").toggle();
+  });
+
+  $("p.NaN").click(function(){
+    $(".NaN-definition").toggle();
+    $(".NaN-name").toggle();
+  });
+
+  $("p.Escape").click(function(){
+    $(".Escape-definition").toggle();
+    $(".Escape-name").toggle();
+  });
+
+  $("p.Alert").click(function(){
+    $(".Alert-definition").toggle();
+    $(".Alert-name").toggle();
+  });
+
+  $("p.Comments").click(function(){
+    $(".Comments-definition").toggle();
+    $(".Comments-name").toggle();
+  });
+
+  $("p.jQuery").click(function(){
+    $(".jQuery-definition").toggle();
+    $(".jQuery-name").toggle();
+  });
+
+  $("p.Attributes").click(function(){
+    $(".Attributes-definition").toggle();
+    $(".Attributes-name").toggle();
+  });
+
+
+
+
+
+
+
+});
